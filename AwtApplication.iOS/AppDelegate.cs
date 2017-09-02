d6d9d1e839b302734738ce11dead18b396677213
finnatinform <finn.zentgraf@inform-software.com>
2017-09-02 22:57:06 +0200

    Backgroundfetch auf ios aktiviert

--- AwtApplication.iOS/AppDelegate.cs
+++ AwtApplication.iOS/AppDelegate.cs
@@ -1,19 +1,16 @@
-using Foundation;
-using UIKit;
-using Xamarin.Forms;
-
-using Lottie.Forms.iOS.Renderers;
-
 using FFImageLoading.Forms.Touch;
-
+using Foundation;
+using Lottie.Forms.iOS.Renderers;
+using ObjCRuntime;
+using System;
+using UIKit;
 using UXDivers.Artina.Shared;
-
 
 namespace AwtApplication.iOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-	[Register ("AppDelegate")]
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register ("AppDelegate")]
 	public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
@@ -34,7 +31,18 @@ namespace AwtApplication.iOS
 
             LoadApplication (new App ());
 
+            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(Params.Constants.CheckNotificationsInterval);
+
 			return base.FinishedLaunching (app, options);
 		}
-	}
+
+        // WTF: [BlockProxy(typeof(NIDActionArity1V137))] 
+        public override void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            base.PerformFetch(application, completionHandler);
+
+            // Entsprechend einbauen statt stop?
+            completionHandler(UIBackgroundFetchResult.NewData);
+        }
+    }
 }
