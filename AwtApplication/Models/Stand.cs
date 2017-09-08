2cc8c25727c076acca423802b7066199e2d65ca6
finnatinform <finn.zentgraf@inform-software.com>
2017-09-08 16:45:11 +0200

    App korrigiert

--- AwtApplication/Models/Stand.cs
+++ AwtApplication/Models/Stand.cs
@@ -22,5 +22,31 @@
             get;
             set;
         }
+        public string GRIAL_ICON
+        {
+            get
+            {
+                switch (LOGO)
+                {
+                    case "School": return GrialShapesFont.School;
+                    case "Dashboard": return GrialShapesFont.Dashboard;
+                    case "Build": return GrialShapesFont.Build;
+                    case "ChatBubble": return GrialShapesFont.ChatBubble;
+                    case "Code": return GrialShapesFont.Code;
+                    case "DesktopMac": return GrialShapesFont.DesktopMac;
+                    case "DesktopWindows": return GrialShapesFont.DesktopWindows;
+                    case "FlashOn": return GrialShapesFont.FlashOn;
+                    case "Group": return GrialShapesFont.Group;
+                    case "Notifications": return GrialShapesFont.Notifications;
+                    case "PhoneAndroid": return GrialShapesFont.PhoneAndroid;
+                    case "PhoneIphone": return GrialShapesFont.PhoneIphone;
+                    case "Place": return GrialShapesFont.Place;
+                    case "Settings": return GrialShapesFont.Settings;
+                    case "ShoppingCart": return GrialShapesFont.ShoppingCart;
+                    case "WebAsset": return GrialShapesFont.WebAsset;
+                    default: return "";
+                }
+            }
+        }
     }
 }
